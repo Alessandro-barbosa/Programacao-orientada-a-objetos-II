b9d2bb1f66c02defa3a1099caee04fdf3bd63c5f
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-02-28 08:50:37 -0300

    Aula Sobre Collections utilizando a List

---
+++ AulaCollection/AgendaTelefonica.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaCollection
+{
+    internal class AgendaTelefonica
+    {
+        List<Contato> listaContatos;
+        public AgendaTelefonica(List<Contato> listaContatos)
+        {
+            listaContatos = new List<Contato>();
+            this.listaContatos = listaContatos;
+        }
+
+        public bool inserir(string nome, string numero)
+        {
+            Contato contato = new Contato(nome,numero);
+            listaContatos.Add(contato);
+            return true;
+        }
+        public string buscarNumero(string telefone)
+        {
+            foreach (var contatos in listaContatos)
+            {
+                if (contatos.telefone.Contains(telefone))
+                    return "Encontrado";                            
+            }           
+                return "Não encontrado";
+        }
+        public void remover(string nome)
+        {
+            foreach (Contato contato in listaContatos)
+            {
+                if (contato.nome.Contains(nome))           
+                    listaContatos.Remove(contato);                
+            }
+        }
+        public int tamanho() { return listaContatos.Count; }
+    }
+}
+//AgendaTelefonica
+//- coleção lista
+
+//+ inserir(nome: String, numero: string _ void
+//+ buscarNumero(nome: string) : string
+//+ remover(nome: string): void
+//+ tamanho(): int
+
+//Contato
+//- nome
+//- telefone
---
+++ AulaCollection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contato> contato = new List<Contato>();
+            AgendaTelefonica agenda = new AgendaTelefonica(contato);          
+            agenda.inserir("ale", "222");
+            agenda.inserir("a", "3");
+            agenda.inserir("b", "4");
+            agenda.inserir("c", "5");
+            agenda.inserir("o", "10");
+            string status = agenda.buscarNumero("10");
+            Console.WriteLine(status);
+            agenda.remover("ale");
+
+            Console.WriteLine(agenda.tamanho());
+
+            Console.ReadLine();
+        }
+    }
+}
