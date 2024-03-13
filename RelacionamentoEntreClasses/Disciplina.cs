6b9e7d0b71e66c01e0d4ddd513554aa306252b0d
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-03-13 08:47:48 -0300

    AtividadeRelacionamentosEntreClasses

---
+++ RelacionamentoEntreClasses/Disciplina.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelacionamentoEntreClasses
+{
+    internal class Disciplina
+    {
+        private int codigo;
+        public string nome { private get; set; }
+        public Professor professor { get; set; }
+        public List<int> preRequisitos { get; set; }
+        public Disciplina(string nome, int codigo){
+            this.nome = nome;
+            this.codigo = codigo;
+            this.preRequisitos = new List<int>();
+        }
+        public void addPreRequisitos(Disciplina c) { 
+            preRequisitos.Add(c.codigo);
+        }
+    }
+}
---
+++ RelacionamentoEntreClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelacionamentoEntreClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> relacaoCodigos = new List<int>();
+            Disciplina introducaoComputacao = new Disciplina("Introducao", 10);
+            Disciplina calculo = new Disciplina("Calculo", 30);
+            Disciplina calculoII = new Disciplina("Calculo II", 30);
+            Disciplina redes = new Disciplina("redes", 20);
+
+            redes.addPreRequisitos(introducaoComputacao);
+            redes.addPreRequisitos(calculo);
+            redes.addPreRequisitos(calculoII);
+
+            Console.WriteLine("Mostrando o código das disciplinas que são pre-requisitos" +
+                "para cursar essa disciplina:");
+            foreach (int codigoDisciplina in redes.preRequisitos){
+                Console.WriteLine($"{codigoDisciplina}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
