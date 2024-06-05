25f67796f8f6334e2368c84d098cf1f57008de08
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-06-05 08:42:56 -0300

    Aula de banco de dados c#
    Conexão com banco de dados, criação de tabelas, pesquisa no banco de dados, fechamento de conexão com banco de dados.

---
+++ DataBase/ConexaoString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    public class ConexaoString
+    {
+        public string  conexaoString = "Data Source=(localdb)\\MSSQLLocalDB;" +
+                "Initial Catalog=Banco00;" +
+                "Integrated Security=True;" +
+                "Connect Timeout=30;" +
+                "Encrypt=False;" +
+                "TrustServerCertificate=False;" +
+                "ApplicationIntent=ReadWrite;" +
+                "MultiSubnetFailover=False";
+    }
+}
---
+++ DataBase/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DataBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConexaoString c = new ConexaoString();
+            SqlConnection conexao;
+            conexao = new SqlConnection(c.conexaoString);
+            try
+            {
+                conexao.Open();
+                /* Parte 2 Inserir Dados 
+                
+                Console.WriteLine("Conexão aberta!");
+
+                var insertCmd = conexao.CreateCommand();
+                insertCmd.CommandText = "INSERT INTO Exemplo (Nome, Sobrenome) VALUES (@nome, @sobrenome);";
+
+                var parmNome = new SqlParameter("@nome", "Maria");
+                var parmSobrenome = new SqlParameter("@sobrenome", "Joaquina");
+
+                insertCmd.Parameters.Add(parmNome);
+                insertCmd.Parameters.Add(parmSobrenome);
+
+                var resultado = insertCmd.ExecuteNonQuery();
+                Console.WriteLine("Linhas afetadas: " + resultado.ToString());
+                */
+                /*Parte 2 Buscar Dados */
+
+                var selectCmd = conexao.CreateCommand();
+
+                selectCmd.CommandText = "SELECT * FROM Exemplo";
+
+                SqlDataReader leitorDados = selectCmd.ExecuteReader();
+
+                while (leitorDados.Read())
+                {
+                    Console.WriteLine("ID: " + leitorDados["Id"] + " " + leitorDados["Nome"] + " " + leitorDados["Sobrenome"]);
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro na conexão");
+            }
+            finally
+            {
+                conexao.Close();
+                Console.WriteLine("conexão fechada");
+            }
+
+
+        }
+    }
+}
